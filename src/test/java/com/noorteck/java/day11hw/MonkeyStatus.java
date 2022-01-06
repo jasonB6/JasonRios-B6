bec5c3b16c7c7e93e5a774ed55a5d5bac27aead9
jason <jason@10.0.0.42>
2022-01-06 18:42:43 -0500

    corrected

--- src/test/java/com/noorteck/java/day11hw/MonkeyStatus.java
+++ src/test/java/com/noorteck/java/day11hw/MonkeyStatus.java
@@ -2,4 +2,22 @@ package com.noorteck.java.day11hw;
 
 public class MonkeyStatus {
 
+	public static void main(String[] args) {
+
+		boolean areWeInTrouble = monkeyTrouble(false, true);
+		System.out.println(areWeInTrouble);
+	}
+
+	public static boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+
+		boolean result = false;
+		if (aSmile == true && bSmile == true) {
+			result = true;
+		} else if (aSmile == false && bSmile == false) {
+			result = true;
+		} else {
+			result = false;
+		}
+		return result;
+	}
 }
