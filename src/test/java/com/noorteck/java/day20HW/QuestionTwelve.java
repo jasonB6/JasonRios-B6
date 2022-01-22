b8a9c9f9fb6d8fab27b90d1462712f7a89750e29
jason <jason@10.0.0.42>
2022-01-21 22:04:13 -0500

    Day 20 HW Q12

---
+++ src/test/java/com/noorteck/java/day20HW/QuestionTwelve.java
@@ -0,0 +1,35 @@
+package com.noorteck.java.day20HW;
+
+public class QuestionTwelve {
+
+	public static void main(String[] args) {
+
+		replaceStr("I cannot do it", "cannot", "can"); // I can do it
+		replaceStr("Tomorrow is my day", "Tomorrow", "Today"); // Today is my day
+
+	}
+
+	public static void replaceStr(String strOne, String oldStr, String newStr) {
+
+		System.out.println(strOne.replace(oldStr, newStr));
+
+	}
+}
+
+/**
+12. Write a method to replace each substring of a given string that matches the given
+regular expression with the given replacement. The method takes 3 String parameters
+and replaces oldStr (parameter 2 ) from strOne (parameter1) with newStr(parameter
+3) and prints the new value
+
+Access Modifier: public
+Non-Access Modifier: static
+Return Type: void
+Method Name: replaceStr
+Parameter1: String strOne
+Parameter2: String oldStr
+Parameter2: String newStr
+Test Data:
+replaceStr (“I cannot do it”, “cannot”, “can” ) ----- I can do it
+replaceStr (“Tomorrow is my day”, “Tomorrow”, “Today” ) ----- Today is my day
+*/
